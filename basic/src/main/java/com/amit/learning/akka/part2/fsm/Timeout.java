6c700f3811af5dba62ed9b105767b221e4d6c732
ubs0085 <amit.lokare-ubs@iconsolutions.com>
2025-07-01 11:03:20 +0100

    Betterment of package

--- basic/src/main/java/com/amit/learning/akka/part2/fsm/Timeout.java
+++ basic/src/main/java/com/amit/learning/akka/part2/fsm/Timeout.java
@@ -2,6 +2,6 @@ package com.amit.learning.akka.part2.fsm;
 
 import akka.actor.typed.Signal;
 
-public record Timeout() implements Signal {
+public record Timeout() implements Signal {//Note timeout is implementing signal
 }
 
