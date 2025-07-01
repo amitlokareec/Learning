ea079a9b2b2ba3350539f0400d5b359a434b74b4
ubs0085 <amit.lokare-ubs@iconsolutions.com>
2025-07-01 09:24:32 +0100

    FIrst Akka code

---
+++ basic/src/main/java/com/amit/learning/akka/ClassicCounter.java
@@ -0,0 +1,48 @@
+package com.amit.learning.akka;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+/**
+ *  In traditional Akka (classic actors), state is stored in mutable instance variables. This can cause race conditions if an actor accidentally exposes state outside.
+ */
+public class ClassicCounter extends AbstractActor {
+    private int count = 0; // A Mutable state. If another thread somehow accesses this, it could lead to inconsistent behavior.
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(Integer.class, msg -> {
+                    count++; // Modifies mutable state
+                    System.out.println("Current Count: " + count);
+                })
+                .build();
+    }
+
+    public static Props props() {
+        return Props.create(ClassicCounter.class);
+    }
+
+    public static void main(String[] args) {
+        // Create the actor system
+        ActorSystem system = ActorSystem.create("CounterSystem");
+
+        // Create the actor
+        ActorRef counter = system.actorOf(ClassicCounter.props(), "classicCounter");
+
+        // Send messages to the actor
+        counter.tell(1, ActorRef.noSender());
+        counter.tell(2, ActorRef.noSender());
+        counter.tell(5, ActorRef.noSender());
+
+        // Gracefully shutdown the system after a delay (to let messages process)
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        system.terminate();
+    }
+}
